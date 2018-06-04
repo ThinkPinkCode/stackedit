72805f5e8f105cc6dacadd5927a0332be66cfd58
Melanie Myers <MMelanieJ@gmail.com>
2018-06-03 22:01:20 -0400

    created export all modal

---
+++ src/components/modals/ExportAllModal.vue
@@ -0,0 +1,67 @@
+<template>
+  <modal-inner aria-label="Export All Files">
+    <div class="modal__content">
+      <p>Please choose a template for your <b>HTML export</b>.</p>
+      <form-entry label="Template">
+        <select class="textfield" slot="field" v-model="selectedTemplate" @keydown.enter="resolve()">
+          <option v-for="(template, id) in allTemplates" :key="id" :value="id">
+            {{ template.name }}
+          </option>
+        </select>
+        <div class="form-entry__actions">
+          <a href="javascript:void(0)" @click="configureTemplates">Configure templates</a>
+        </div>
+      </form-entry>
+    </div>
+    <div class="modal__button-bar">
+      <button class="button button--copy">Copy to clipboard</button>
+      <button class="button" @click="config.reject()">Cancel</button>
+      <button class="button" @click="resolve()">Ok</button>
+    </div>
+  </modal-inner>
+</template>
+
+<script>
+  import Clipboard from 'clipboard';
+  import exportSvc from '../../services/exportSvc';
+  import modalTemplate from './common/modalTemplate';
+
+  export default modalTemplate({
+    data: () => ({
+      result: '',
+    }),
+    computedLocalSettings: {
+      selectedTemplate: 'htmlExportTemplate',
+    },
+    mounted() {
+      let timeoutId;
+      this.$watch('selectedTemplate', (selectedTemplate) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(async () => {
+          const currentFile = this.$store.getters['file/current'];
+          const html = await exportSvc.applyTemplate(
+            currentFile.id,
+            this.allTemplates[selectedTemplate],
+          );
+          this.result = html;
+        }, 10);
+      }, {
+        immediate: true,
+      });
+      this.clipboard = new Clipboard(this.$el.querySelector('.button--copy'), {
+        text: () => this.result,
+      });
+    },
+    destroyed() {
+      this.clipboard.destroy();
+    },
+    methods: {
+      resolve() {
+        const { config } = this;
+        const currentFile = this.$store.getters['file/current'];
+        config.resolve();
+        exportSvc.exportToDisk(currentFile.id, 'html', this.allTemplates[this.selectedTemplate]);
+      },
+    },
+  });
+</script>
